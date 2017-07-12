d6252e848db3ca5bd3e8dcf6dccc3efa27277126
tuffery <furtuff@gmail.com>
2017-07-12 19:04:57 +0200

    trying to get issues from GithubApi

--- Scyansis-server.js
+++ Scyansis-server.js
@@ -6,8 +6,21 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.get('/:username', function(req, res) {
+	var getIssuesRequest = require('request');
+	getIssuesRequest({
+	    headers: {
+	       'Content-Type': 'application/vnd.github.v3+json'
+	    },
+	    uri: 'https://api.github.com/'+req.params.username+'/issues',
+	    method: 'GET'
+	  }, function (err, response, body) {
+		  console.log('errors' +err);
+		  console.log('response' +response);
+		  console.log('body' +body);
+
+	  });
+    res.json({ message: 'hooray! welcome to our api!' +req.params.username});   
 });
 
 app.use('/issues/', router);
